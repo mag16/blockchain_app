a4e645693dadcd8001b588620829b5f7e2d6ecf6
mag16 <marcoguzman16@gmail.com>
2021-05-05 14:56:26 -0700

    progress on blockChain application to GH

---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.0;
+
+import "./Token.sol";
+
+contract EthSwap {
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
+
+    event TokenPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable{
+        //Calculate the number of tokens to buy
+        uint tokenAmount = msg.value * rate;
+        
+        //Require that EthSwap has enough tokens
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        // Transfer token to the user
+        token.transfer(msg.sender, tokenAmount);
+
+        // Emit an event
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint _amount) public {
+        //calculate the amount of ether to redeem
+        uint etherAmount = _amount / rate;
+
+        // Perform sale
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+    }
+
+
+
+
+
+
+}
+
