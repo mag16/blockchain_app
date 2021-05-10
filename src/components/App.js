f2b1c64cc6ebdad510200571cddfaec9895233a8
mag16 <marcoguzman16@gmail.com>
2021-05-10 13:48:09 -0700

    loading data to page

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Web3 from "web3";
-import NavBar from './NavBar';
-import './App.css';
+import Token from "../abis/Token.json";
+import EthSwap from "../abis/EthSwap.json";
+import NavBar from "./NavBar";
+import Main from "./Main";
+import "./App.css";
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
 
-    const accounts = await web3.eth.getAccounts()
-    
-    this.setState({ account: accounts[0] })
-    
-    const ethBalance = await web3.eth.getBalance(this.state.account)
-    this.setState({ ethBalance: ethBalance })
-    console.log(this.state.ethBalance);
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ account: accounts[0] });
+
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+
+    // Load Token
+    const networkId = await web3.eth.net.getId();
+    const tokenData = Token.networks[networkId]
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address);
+      this.setState({ token })
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+      this.setState({tokenBalance: tokenBalance.toString()})   
+    } else {
+      window.alert('Token contract not deployed to detected network')
+    }
+
+    // Load EthSwap
+    const ethSwapData = EthSwap.networks[networkId]
+    if (ethSwapData) {
+      const ethSwap = new web3.eth.Contract(Token.abi, tokenData.address);
+      this.setState({ ethSwap })  
+    } else {
+      window.alert('EthSwap contract not deployed to detected network.')
+    }
+    this.setState({loading: false})
   }
 
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected.  You should consider trying MetaMast!"
+      )
     }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)  
-    }
-    else {
-      window.alert('Non-Ethereum browser detected.  You should consider trying MetaMast!')
-    }  
   }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       account: '',
-      ethBalance: '0'
-    }
+      token: {},
+      ethSwap: {},
+      ethBalance: '0',
+      tokenBalance: '0',
+      loading: true
+    };
   }
 
   render() {
+    let content;
+    if (this.state.loading) {
+      content = <p id="loader" className="text-center">Loading...</p>
+    } else {
+      content = <Main />
+    }
+
     return (
       <div>
-        <NavBar account={this.state.account }/>
+        <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -55,11 +89,10 @@ class App extends Component {
                   href="http://www.dappuniversity.com/bootcamp"
                   target="_blank"
                   rel="noopener noreferrer"
-                >
+                >             
                 </a>
-                <h1>Hello Guzmaniac's</h1>
-                
-             </div>
+                {content}
+              </div>
             </main>
           </div>
         </div>
