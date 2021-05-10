eb5e04733a9f967104aaa0b18ad5725e8c397d39
mag16 <marcoguzman16@gmail.com>
2021-05-10 14:21:25 -0700

    buy, sell form, README

--- src/components/App.js
+++ src/components/App.js
@@ -75,7 +75,10 @@ class App extends Component {
     if (this.state.loading) {
       content = <p id="loader" className="text-center">Loading...</p>
     } else {
-      content = <Main />
+      content = <Main
+        ethBalance={this.state.ethBalance}
+        tokenBalance={this.state.tokenBalance}
+      />
     }
 
     return (
@@ -83,7 +86,7 @@ class App extends Component {
         <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style ={{ maxWidth: '600px'}}>
               <div className="content mr-auto ml-auto">
                 <a
                   href="http://www.dappuniversity.com/bootcamp"
@@ -91,7 +94,11 @@ class App extends Component {
                   rel="noopener noreferrer"
                 >             
                 </a>
+
+
                 {content}
+
+
               </div>
             </main>
           </div>
