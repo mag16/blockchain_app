78d18ae74fa04aee24ccbf5ea0fd8d8f573c0e16
mag16 <marcoguzman16@gmail.com>
2021-05-10 17:36:29 -0700

    fixes

--- src/components/App.js
+++ src/components/App.js
@@ -24,25 +24,27 @@ class App extends Component {
 
     // Load Token
     const networkId = await web3.eth.net.getId();
-    const tokenData = Token.networks[networkId]
+    const tokenData = Token.networks[networkId];
     if (tokenData) {
       const token = new web3.eth.Contract(Token.abi, tokenData.address);
-      this.setState({ token })
-      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
-      this.setState({tokenBalance: tokenBalance.toString()})   
+      this.setState({ token });
+      let tokenBalance = await token.methods
+        .balanceOf(this.state.account)
+        .call();
+      this.setState({ tokenBalance: tokenBalance.toString() });
     } else {
-      window.alert('Token contract not deployed to detected network')
+      window.alert("Token contract not deployed to detected network");
     }
 
     // Load EthSwap
-    const ethSwapData = EthSwap.networks[networkId]
+    const ethSwapData = EthSwap.networks[networkId];
     if (ethSwapData) {
       const ethSwap = new web3.eth.Contract(Token.abi, tokenData.address);
-      this.setState({ ethSwap })  
+      this.setState({ ethSwap });
     } else {
-      window.alert('EthSwap contract not deployed to detected network.')
+      window.alert("EthSwap contract not deployed to detected network.");
     }
-    this.setState({loading: false})
+    this.setState({ loading: false });
   }
 
   async loadWeb3() {
@@ -53,32 +55,65 @@ class App extends Component {
       window.web3 = new Web3(window.web3.currentProvider);
     } else {
       window.alert(
-        "Non-Ethereum browser detected.  You should consider trying MetaMast!"
-      )
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
     }
   }
+
+  buyTokens = (etherAmount) => {
+    this.setState({ loading: true })
+    this.state.ethSwap.methods
+      .buyTokens()
+      .send({ value: etherAmount, from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.setState({ loading: false })
+      })
+  }
+
+  sellTokens = (tokenAmount) => {
+    this.setState({ loading: true });
+    this.state.token.methods
+      .approve(this.state.ethSwap.address, tokenAmount)
+      .send({ from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.state.ethSwap.methods
+          .sellTokens(tokenAmount)
+          .send({ from: this.state.account })
+          .on("transactionHash", (hash) => {
+            this.setState({ loading: false });
+          });
+      });
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      account: '',
+      account: "",
       token: {},
       ethSwap: {},
-      ethBalance: '0',
-      tokenBalance: '0',
-      loading: true
+      ethBalance: "0",
+      tokenBalance: "0",
+      loading: true,
     };
   }
 
   render() {
     let content;
     if (this.state.loading) {
-      content = <p id="loader" className="text-center">Loading...</p>
+      content = (
+        <p id="loader" className="text-center">
+          Loading...
+        </p>
+      );
     } else {
-      content = <Main
-        ethBalance={this.state.ethBalance}
-        tokenBalance={this.state.tokenBalance}
-      />
+      content = (
+        <Main
+          ethBalance={this.state.ethBalance}
+          tokenBalance={this.state.tokenBalance}
+          buyTokens={this.buyTokens}
+          sellTokens={this.sellTokens}
+        />
+      );
     }
 
     return (
@@ -86,20 +121,12 @@ class App extends Component {
         <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style ={{ maxWidth: '600px'}}>
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >             
-                </a>
-
-
-                {content}
-
-
-              </div>
+            <main
+              role="main"
+              className="col-lg-12 ml-auto mr-auto"
+              style={{ maxWidth: "600px" }}
+            >
+              <div className="content mr-auto ml-auto">{content}</div>
             </main>
           </div>
         </div>
