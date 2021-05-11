595a839e3a7b26ec225986f1c54fd3cb5f74d24b
mag16 <marcoguzman16@gmail.com>
2021-05-10 18:05:23 -0700

    Updates

---
+++ precache-manifest.a18a245e1af8b71eafb2cbc804d24dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac8a23a9cc6b5a3b73e0",
+    "url": "/mag16/blockchain_app/static/js/runtime~main.ac8a23a9.js"
+  },
+  {
+    "revision": "8d0da1072086e9480bf1",
+    "url": "/mag16/blockchain_app/static/js/main.8d0da107.chunk.js"
+  },
+  {
+    "revision": "13cc3f278c479e7b8989",
+    "url": "/mag16/blockchain_app/static/js/1.13cc3f27.chunk.js"
+  },
+  {
+    "revision": "8d0da1072086e9480bf1",
+    "url": "/mag16/blockchain_app/static/css/main.d0b452c5.chunk.css"
+  },
+  {
+    "revision": "13cc3f278c479e7b8989",
+    "url": "/mag16/blockchain_app/static/css/1.5ae43c69.chunk.css"
+  },
+  {
+    "revision": "c3f826cd49cea639be0d44f41873a1fe",
+    "url": "/mag16/blockchain_app/index.html"
+  }
+];
